b34774c1a7068dd2fd929592f4e8fb9346d9fc40
Matheus Marques <matheusmarquesportela@gmail.com>
2025-09-22 16:36:51 -0300

    atualização Testes

--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -14,7 +14,11 @@ class UserRegistrationTest extends TestCase
     public function test_example(): void
     {
         $response = $this->get('/');
+        $response->assertStatus(200);
+    }
 
+    public function test_the_application_returns_a_successful_response(): void {
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
 }
