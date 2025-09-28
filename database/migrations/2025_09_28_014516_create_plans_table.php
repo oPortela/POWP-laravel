80d43b955be9e285c0a9847b2a0e648f253e3d5c
Matheus Marques <118008062+oPortela@users.noreply.github.com>
2025-09-28 13:23:30 -0300

    Novas migrations
    Migrations de plano e contato

---
+++ database/migrations/2025_09_28_014516_create_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('planos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100)->unique();
+            $table->text('descricao')->nullable();
+            $table->decimal('valor', 8, 2);
+            $table->boolean('ativo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('planos');
+    }
+};
